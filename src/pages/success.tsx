8bd23faae38414d1e8aa28a863c92d2f83977676
Heril Saha <herilsaha06@gmail.com>
2021-10-03 04:16:18 -0600

    fix(form): router on success.tsx

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,13 @@
-const Submission = () => <div>Success</div>;
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const Submission = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/?success=true');
+  }, [router]);
+
+  return <div />;
+};
 
 export default Submission;
